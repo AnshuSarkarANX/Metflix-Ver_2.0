41378eade4079b64bdfe4d8fa32a927a2300b417
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-02-25 11:30:54 +0530

    Updated

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -14,6 +14,6 @@ export const userSlice = createSlice({
   },
 });
 
-export const { login, logout,loginskip } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 export const selectUser = (state) => state.user.user;
 export const userReducer = userSlice.reducer;
