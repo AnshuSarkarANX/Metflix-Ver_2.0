4bd55d80d350de0a80cc05b0e68a80494a43dca2
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-02-27 00:47:47 +0530

    `feat : ` TeailerScreen and Mouseover Trailer

--- src/Screens/HomeScreen/Row.jsx
+++ src/Screens/HomeScreen/Row.jsx
@@ -1,25 +1,67 @@
 import  { useState, useEffect } from "react";
-import axios from "../../axios";
+import Axios from "../../axios";
+import axios from "axios";
+import {useNavigate} from 'react-router-dom';
+import { ytKey1,ytKey2,ytKey3 } from "../../request";
 import "./Row.css"; 
+import YoutubePlayer from "./TrailerFeature/YoutubePlayer";
 const img_url = "https://image.tmdb.org/t/p/w300";
+
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [video,setVideo] = useState(null);
+  let mouseOverTimeout;
+  const Navigate = useNavigate();
   useEffect(() => {
     (async function fetchData() {
-      const request = await axios.get(fetchUrl);
+      const request = await Axios.get(fetchUrl);
       setMovies(request.data.results); 
     })()
   }, [fetchUrl]);
+  async function trailer(query) { 
+    let request = {
+      method: "get",
+      url: `https://www.googleapis.com/youtube/v3/search?q=${query} netflix official trailer&part=snippet&videoDuration=short&type=video&maxResults=1&key=${ytKey2}`,
+    };
+    const response = await axios(request);
+    return response.data.items[0];
+   }
+   const isMobileDevice = () =>{
+    return /Mobi|Android/i.test(navigator.userAgent);
+   }
+   const handleMouseOver = async (movie) =>{
+    clearTimeout(mouseOverTimeout);
+  mouseOverTimeout = setTimeout(async () =>{
+         const response = await trailer(
+           movie.original_name || movie.original_title
+         );
+         setVideo(response);
+    },1000)
+   }
+   const handleMouseOut = ()=>{
+    clearTimeout(mouseOverTimeout);
+    setVideo(null);
+   }
+   const handleTouchStart = async (movie) => {
+            const response = await trailer(
+              movie.original_name || movie.original_title
+            );
+            setVideo(response);
+    Navigate("/trailerscreen", { state: { movie,  video:response } });
+   } 
   return (
     <div className="row">
       <h2 className="RowTitle">{title}</h2>
+      {video &&  (
+        <YoutubePlayer videoId={video.id.videoId} />
+      )}
       <div className="row_posters">
         {movies.map((movie) => (
           <img
-            onMouseOver={() =>
-              console.log(`${movie.original_name || movie.original_title}`)
-            }
             key={movie.id}
+            onMouseOver={isMobileDevice() ? null : () => handleMouseOver(movie)}
+            onMouseOut={handleMouseOut}
+            onTouchStart={isMobileDevice()?() => handleTouchStart(movie):null}
             className={`row_image ${isLargeRow && "row_imageLarge"}`}
             src={`${img_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
